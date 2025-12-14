1c403f34999b234da61d05f2cb6c02cc65a68ef1
DARK LEGEND <soffnekos@gmail.com>
2025-12-14 14:47:24 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -482,15 +482,15 @@ global.dfail = (type, m, conn, usedPrefix, command) => {
     let verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom();
 
     const msg = {
-        rowner: '「🩵」Este comando solo puede ser usado por mi creador.\n\n> DuarteXV,
-        owner: '「💎」Este comando solo puede ser usado por Duarte',
-        premium: '「🩵」 Este comando solo puede ser usado por los usuarios premiums.',
-        botprem: '「💥」Este comando solo está disponible para bots premium.',
-        private: '「💎」Este comando solo puede ser usado en chats privados.',
-        admin: '「🩵」Este comando solo puede ser usado por admins.',
-        botAdmin: '「💎」Para usar este comando, debo ser admin del grupo.',
-        unreg: '「🩵」¡Hey! no estas registrado, registrate para usar mis comandos\n\n/Reg nombre.edad\n\n! Ejemplo: _/Reg Duarte.14_',
-        restrict: '「💎」Este comando fue desactivado por mi Creador\n\n> DuarteXV.'
+        rowner: '「」Este comando solo puede ser usado por mi creador.\n\n> KJYOTAKA AYANOKOUJI,
+        owner: '「」Este comando solo puede ser usado por KJYOTAKA AYANOKOUJI',
+        premium: '「」 Este comando solo puede ser usado por los usuarios premiums.',
+        botprem: '「」Este comando solo está disponible para bots premium.',
+        private: '「」Este comando solo puede ser usado en chats privados.',
+        admin: '「」Este comando solo puede ser usado por admins.',
+        botAdmin: '「」Para usar este comando, debo ser admin del grupo.',
+        unreg: '「」¡Hey! no estas registrado, registrate para usar mis comandos\n\n/Reg nombre.edad\n\n! Ejemplo: _/Reg Duarte.14_',
+        restrict: '「」Este comando fue desactivado por mi Creador\n\n> KJYOTAKA AYANOKOUJI.'
     }[type];
 
     if (msg)
